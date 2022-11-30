16ac439766686ab9feff3884688159421dffc09d
vignesh kannaa <vigneshkannaa1996@gmail.com>
2022-11-30 18:50:17 +0000

    updating project section with modal

--- src/HomePage/Project.tsx
+++ src/HomePage/Project.tsx
@@ -1,16 +1,135 @@
-import { IonCard, IonChip, IonCol, IonGrid, IonIcon, IonRow } from '@ionic/react'
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonChip,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonRow,
+  IonTitle,
+} from '@ionic/react'
 import { logoGithub, logoGooglePlaystore } from 'ionicons/icons'
+import { useState } from 'react'
+import { FaJava ,FaPython,FaAngular} from "react-icons/fa";
+import { GrMysql} from "react-icons/gr";
+import Modal from '../components/Modal'
+
 import './Project.css'
 
 const Project = () => {
+  const [isShown, setIsShown] = useState(false)
+  const [projectClicked, setProjectClicked] = useState('')
+
   const gitHub = 'https://github.com/vignesh-kannaa'
   const gitHubCreditRiskPredict = gitHub + '/ml-credit_risk_prediction'
+  const gitHubUnsupuervisedLearning = gitHub + '/ml-unsupervised-customer-segmentation'
   const playStoreSpotaround = 'https://play.google.com/store/apps/details?id=com.spotaround'
   return (
     <div id="project" className="section_margin">
       <h1>Projects</h1>
       <hr></hr>
       <IonGrid>
+        {isShown && <Modal title={projectClicked} closeModal={() => setIsShown(false)} />}
+        <IonRow>
+          <IonCol size="12" sizeMd="6">
+            <IonCard
+              className="bg section-margin-desc"
+              onClick={() => {
+                setIsShown(true)
+                setProjectClicked('customerSegmentation')
+              }}
+            >
+              <img src={require('../assets/images/customerSegmentation.jpeg')} />
+              <IonCardHeader>
+                <IonRow>
+                  <IonTitle className="title">Customer Segmentation</IonTitle>
+                  <a target="_blank" href={gitHubUnsupuervisedLearning}>
+                    <IonIcon className="ion-icon-project" icon={logoGithub}></IonIcon>
+                  </a>
+                </IonRow>
+                <IonCardSubtitle>
+                  <IonChip className="ion-chip ">
+                  <FaPython className='chipIcon'/> Python
+                  </IonChip>
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent className="content">
+                Unsupervised Machine learning models for customer segmentation using mall customer
+                data
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+          <IonCol size="12" sizeMd="6">
+            <IonCard
+              className="bg mar-positive"
+              onClick={() => {
+                setIsShown(true)
+                setProjectClicked('creditRiskPrediction')
+              }}
+            >
+              <img src={require('../assets/images/creditrisk.jpg')} />
+              <IonCardHeader>
+                <IonRow>
+                  <IonTitle className="title">Credit Risk Prediction</IonTitle>
+                  <a target="_blank" href={gitHubCreditRiskPredict}>
+                    <IonIcon className="ion-icon-project" icon={logoGithub}></IonIcon>
+                  </a>
+                </IonRow>
+                <IonCardSubtitle>
+                  <IonChip className="ion-chip ">
+                  <FaPython className='chipIcon'/> Python
+                  </IonChip>
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent className="content">
+                Supervised Machine Learning Classifier models to predict credit risk
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol size="12" sizeMd="6">
+            <IonCard
+              className="bg mar-negative"
+              onClick={() => {
+                setIsShown(true)
+                setProjectClicked('spotAround')
+              }}
+            >
+              <img src={require('../assets/images/spotaround.png')} />
+              <IonCardHeader>
+                <IonRow>
+                  <IonTitle className="title">Spot Around</IonTitle>
+                  <a target="_blank" href={gitHub}>
+                    <IonIcon className="ion-icon-project" icon={logoGithub}></IonIcon>
+                  </a>
+                  <a target="_blank" href={playStoreSpotaround}>
+                    <IonIcon className="ion-icon-project" icon={logoGooglePlaystore}></IonIcon>
+                  </a>
+                </IonRow>
+                <IonCardSubtitle>
+                  <IonChip className="ion-chip ">
+                    <FaJava className='chipIcon'/>
+                    Java
+                  </IonChip>
+                  <IonChip className="ion-chip">
+                    <FaAngular className='chipIcon'/>Angular
+                  </IonChip>
+                  <IonChip className="ion-chip">
+                  <GrMysql  className='chipIcon'/>
+                  Mysql</IonChip>
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent className="content">
+                Mobile App that will help to spot local freelancers anywhere
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      {/* <IonGrid>
         <IonRow>
           <IonCol size="12" sizeMd="6">
             <IonCard className="card section-margin-desc">
@@ -68,7 +187,7 @@ const Project = () => {
             </IonCard>
           </IonCol>
         </IonRow>
-      </IonGrid>
+      </IonGrid> */}
     </div>
   )
 }
---
+++ src/constants/projectInfo.ts
@@ -0,0 +1,23 @@
+export const customerSegmentation = {
+  title: 'Customer Segmentation',
+  images: ['customerSegmentation.jpeg', 'customerSegmentation.jpeg'],
+  category: 'ML - Unsupervised',
+  projectUrl: 'https://github.com/vignesh-kannaa/ml-unsupervised-customer-segmentation',
+  description: 'Customer segmentation using Unspervised algorithms such as K means, Hierarchical ',
+}
+export const creditRiskPrediction = {
+  title: 'Credit Risk Prediction',
+  images: ['creditrisk.jpg', 'creditrisk.jpg'],
+  category: 'ML - Supervised',
+  projectUrl: 'https://github.com/vignesh-kannaa/ml-credit_risk_prediction',
+  description:
+    'Prediction of credit card risk using supervised learning like random forest , support vector, MLP and finding the efficient method',
+}
+export const spotAround = {
+  title: 'Spot Around',
+  images: ['spotaround.png', 'spotaround.png'],
+  category: 'Mobile App',
+  projectUrl: 'https://github.com/vignesh-kannaa/SpotAround',
+  description:
+    'Mobile application that will help to spot local freelancers anywhere and anytime. Schedule appointments with the freelancers',
+}
