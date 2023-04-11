0bd037ac6569db43c8bc3a7830ba733845d7a38f
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-04-11 15:09:29 +0100

    adding animation

--- src/HomePage/AboutMe.tsx
+++ src/HomePage/AboutMe.tsx
@@ -9,11 +9,7 @@ const AboutMe = () => {
       <IonRow className="section-margin-desc">
         <IonCol size="12" sizeMd="6">
           <h3>
-            I'm currently pursuing my master's in <strong>Artificial Intelligence</strong> at{' '}
-            <a href="https://www.aston.ac.uk/" target="_blank">
-              <strong>Aston University</strong>
-            </a>
-            , Birmingham.
+            Unlocking Business Potential with Data and AI Innovation
             <br />
             <br />
             Possesses <strong>4+ years</strong> of Software experience working in large and small
@@ -22,7 +18,7 @@ const AboutMe = () => {
             environments.
           </h3>
         </IonCol>
-        <IonCol size="12" sizeMd="6"  >
+        <IonCol size="12" sizeMd="6">
           <div className="ion-text-center">
             <img className="image" src={require('../assets/images/vignesh.jpg')} />
           </div>
--- src/HomePage/Intro.tsx
+++ src/HomePage/Intro.tsx
@@ -1,19 +1,21 @@
-import "./Intro.css";
+import './Intro.css'
+import { TypeAnimation } from 'react-type-animation'
 
 const Intro = () => {
   return (
     <div id="about" className="section_margin intro-content">
-      <img
-        className="bgImage"
-        src={require("../assets/images/bannerImg.gif")}
-      />
+      <img className="bgImage" src={require('../assets/images/bannerImg.gif')} />
       <div className="herocontent">
-        {/* <img src={require("../assets/images/memoji.png")} /> */}
         <h2>Hello World, I'm</h2>
         <div className="header">Vignesh Kannaa</div>
-        <div className="intro-desc">Software Engineer | Machine Learning</div>
+        <div className="intro-desc">
+          <TypeAnimation
+            sequence={['Software Developer', 1000, 'Machine Learning Engineer', 1000]}
+            repeat={Infinity}
+          />
+        </div>
       </div>
     </div>
-  );
-};
-export default Intro;
+  )
+}
+export default Intro
