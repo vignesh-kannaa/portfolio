3054f3d8195cf22fc8e0f04e87da7d4dda3e7191
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-06-17 10:51:42 +0100

    reverting old animation for banner image

--- src/HomePage/Intro.tsx
+++ src/HomePage/Intro.tsx
@@ -6,32 +6,33 @@ import { TypeAnimation } from 'react-type-animation'
 const Intro = () => {
   return (
     <div id="intro" className="section_margin intro-content">
-      {/* <img className="bgImage" src={require('../assets/images/bannerImg.gif')} /> */}
-      {/* <div className="herocontent"> */}
-      <IonGrid>
+      <img className="bgImage" src={require('../assets/images/bannerImg.gif')} />
+      <div className="herocontent">
+        {/* <IonGrid>
         <IonRow>
-          <IonCol size="12" sizeMd="7">
-            <h2>Hello World, I'm</h2>
-            <div className="header">Vignesh Kannaa</div>
-            <div className="intro-desc">
-              <TypeAnimation
-                sequence={[
-                  'Machine Learning Engineer',
-                  1500,
-                  'Data Scientist',
-                  1500,
-                  'Software Developer',
-                  1500,
-                ]}
-                repeat={Infinity}
-              />
-            </div>
-          </IonCol>
+          <IonCol size="12" sizeMd="7"> */}
+        <h2>Hello World, I'm</h2>
+        <div className="header">Vignesh Kannaa</div>
+        <div className="intro-desc">
+          <TypeAnimation
+            sequence={[
+              'Machine Learning Engineer',
+              1500,
+              'Data Scientist',
+              1500,
+              'Software Developer',
+              1500,
+            ]}
+            repeat={Infinity}
+          />
+        </div>
+        {/* </IonCol>
           <IonCol className="robo" size="12" sizeMd="5">
             <RobotCanvas />
-          </IonCol>
+          </IonCol> 
         </IonRow>
-      </IonGrid>
+      </IonGrid>*/}
+      </div>
     </div>
   )
 }
